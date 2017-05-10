1e03f0e9260905c0e2c118a651d39bb0dd3794e3
Sun Zhi <lestdawn@163.com>
2017-05-10 21:26:31 +0800

    添加项目文件。

---
+++ Define.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define  ID          "111111112"        // your id
+#define  PASSWORD    "123456"           // your password
+#define  SERVER_IP   "127.0.0.1"	    // server ip
+#define  SERVER_PORT 10087				// server port
---
+++ Gobang.h
@@ -0,0 +1,39 @@
+#pragma once
+
+void authorize(const char *id, const char *pass);
+
+void gameStart();
+
+void gameOver();
+
+void roundStart(int round);
+
+void oneRound();
+
+void roundOver(int round);
+
+int observe();
+
+void putDown(int row, int col);
+
+void noStep();
+
+void step();
+
+void saveChessBoard();
+
+struct Situ {
+	int win;
+	int live4;
+	int live3;
+	int live2;
+	int live1;
+	int sleep4;
+	int sleep3;
+	int sleep2;
+	int sleep1;
+	int die;
+	Situ() {
+		live1 = 0; live2 = 0; live3 = 0; live4 = 0; sleep4 = 0; sleep3 = 0; sleep2 = 0; sleep1 = 0; win = 0; die = 0;
+	}
+};
